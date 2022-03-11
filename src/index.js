635ed1b0b737450e9ffde602577980d7b9338227
Muhammad Asif <muhammadasifhaqnawaz2019@gmail.com>
2022-03-12 01:10:13 +0500

    Class life methods explained

--- src/index.js
+++ src/index.js
@@ -1,17 +1,91 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class ErrorCatcher extends Component {
+  state = { error: null };
+
+  componentDidCatch(error, info) {
+    console.log('[componentDidCatch]', error);
+    this.setState({ error: info.componentStack });
+  }
+
+  render() {
+    if (this.state.error) {
+      return (
+        <div>
+          An error occurred: { this.state.error }
+        </div>
+      )
+    }
+    return this.props.children;
+  }
+}
+
+class LifeCycleDemo extends Component {
+  state = { counter: 0 };
+  constructor(props) {
+    super(props);
+    console.log('[constructor]');
+    console.log(' State already set:', this.state);
+  }
+  componentDidMount() {
+    console.log('[componentDidMount]', 'Mounted');
+  }
+  static getDerivedStateFromProps(nextProps, prevProps) {
+    console.log('[getDerivedStateFromProps]');
+    console.log(' Next props:', nextProps);
+    console.log(' Prev state:', prevProps);
+  }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[shouldComponentUpdate]', 'Deciding to update');
+    return true;
+  }
+  getSnapshotBeforeUpdate(nextProps, nextState) {
+    console.log('[getSnapshotBeforeUpdate]', 'About to update...');
+    return `Time is ${Date.now()}`;
+  }
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('[componentDidUpdate]', 'Updated.');
+    console.log('  snapshot:', snapshot);
+  }
+  componentWillUnmount() {
+    console.log('[componentWillUnmount]', 'Goodbye cruel world.');
+  }
+
+  handleClick = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  };
+  causeErrorNextRender = () => {
+    this.setState({
+      causeError: true
+    });
+  };
+
+  render() {
+    console.log('[render]');
+    if (this.state.causeError) {
+      throw new Error('oh no!!');
+    }
+
+    return (
+      <div>
+        <span>Counter: {this.state.counrer}</span>
+        <button onClick={this.handleClick}>
+          Click to increment
+        </button>
+        <button onClick={this.causeErrorNextRender}>
+          Throw an error
+        </button>
+      </div>
+    );
+  }
+
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <ErrorCatcher>
+    <LifeCycleDemo />
+  </ErrorCatcher>
+  , document.querySelector('#root'));
